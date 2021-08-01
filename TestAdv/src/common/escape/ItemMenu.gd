004be2177a9125656cf722d9eb7be300c68863f3
Syunsuke Ozeki <2dgames.jp@gmail.com>
2021-08-01 14:01:17 +0900

    アイテムメニュー実装中

---
+++ TestAdv/src/common/escape/ItemMenu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var _closed = false
+
+func _ready() -> void:
+	if AdvConst.DEBUG:
+		# TODO: ウィンドウをリサイズ
+		OS.set_window_size(Vector2(853, 480))
+		#OS.set_window_size(Vector2(480, 270))
+
+
+func _process(delta: float) -> void:
+	if _closed:
+		queue_free()
+
+
+func _on_Button_pressed() -> void:
+	_closed = true
