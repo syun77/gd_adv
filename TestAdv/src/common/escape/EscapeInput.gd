7737b346796ec5ab37f8ab0f4d50a856fa3d48e7
Syunsuke Ozeki <2dgames.jp@gmail.com>
2021-08-10 16:39:57 +0900

    入力メニューをいったん実装

--- TestAdv/src/common/escape/EscapeInput.gd
+++ TestAdv/src/common/escape/EscapeInput.gd
@@ -1,27 +1,52 @@
 extends Control
 
-const PRESS_OFS_Y = 8.0
+class_name EscapeInputObj
 
-var _idx = 0
-var _char_tbl = []
-var _label_pos:Vector2
-var _label_yofs:float = 0.0
+const PRESS_OFS_Y = 8.0 # アニメーションでずらす値
+const WIDTH = 96
 
+var _idx = 0 # 選んでいる項目番号
+var _char_tbl:PoolStringArray # 表示する文字の配列
+var _label_pos:Vector2 # 文字の初期位置
+var _label_yofs:float = 0.0 # 文字をアニメーションでずらす値
+var _is_blink:bool = false
+var _bg_color:Color
+var _timer:float = 0
+
+onready var _bg = $Bg
 onready var _label = $Bg/Label
 
-func _ready() -> void:
-	_label_pos = _label.rect_position
+# 開始パラメータを設定
+func start(tbl:PoolStringArray, idx:int) -> void:
+	_char_tbl = tbl
+	_idx = idx # 初期位置
+
+# 点滅開始
+func start_blink() -> void:
+	_bg_color = _bg.color
+	_is_blink = true
+
+# 選択している番号を取得する
+func get_idx() -> int:
+	return _idx
 	
-	# テストデータ
-	_char_tbl = [
-		"1",
-		"2",
-		"3",
-		"4",
-		"5",
-	]
+# ボタンを非表示にする
+func hide_button() -> void:
+	$UpButton.hide()
+	$DownButton.hide()
+
+func _ready() -> void:
+	#if Global.initialized() == false:
+	#	# 初期化していなかったら初期化してしまう
+	#	Global.init()
+	
+	# 初期位置を保存
+	_label_pos = _label.rect_position
 
 func _process(delta: float) -> void:
+	_timer += delta
+	
+	# 値の領域外チェック
 	var num = _char_tbl.size()
 	if _idx < 0:
 		_idx = num - 1
@@ -36,13 +61,22 @@ func _process(delta: float) -> void:
 	_label.rect_position = _label_pos + Vector2(0, _label_yofs)
 
 	# テキストを設定
-	_label.text = _char_tbl[_idx]
+	if _idx >= _char_tbl.size():
+		_label.text = "壱"
+	else:
+		_label.text = _char_tbl[_idx]
+	
+	if _is_blink:
+		# 点滅させる
+		_bg.color = _bg_color.linear_interpolate(Color.gray, abs(sin(_timer*4)))
 
 func _on_UpButton_pressed() -> void:
+	# 上ボタンを押した
 	_idx -= 1
 	_label_yofs = -PRESS_OFS_Y
 
 
 func _on_DownButton_pressed() -> void:
+	# 下ボタンを押した
 	_idx += 1
 	_label_yofs = PRESS_OFS_Y
---
+++ TestAdv/src/common/escape/EscapeInputMenu.gd
@@ -0,0 +1,153 @@
+extends Control
+
+class_name EscapeInputMenuObj
+
+# 入力オブジェクト
+const EscapeInput = preload("res://src/common/escape/EscapeInput.tscn")
+
+const DIAL_TOP_Y = 120 # 入力オブジェクトのY座標
+const DIAL_MARGIN_W = 16
+const TIMER_CORRECT = 1.5
+
+# 動作モード
+enum eMode {
+	NUM, # 数値モード
+	KANA, # 文字モード
+	PIC, # 画像モード
+}
+
+# 状態
+enum eState {
+	INIT, # 初期化
+	MAIN, # メイン
+	CORRECT, # 正解
+	END, # 終了
+}
+
+var _mode:int = eMode.NUM # 動作モード
+var _state:int = eState.INIT # 初期化
+var _timer:float = 0
+var _digit:int = 0 # 桁数
+var _var_idx:int = 0 # 保存する変数番号
+var _answer_num:int = 0 # 数値の場合の正解
+var _auto_check:bool = false # 自動で正解チェックを行うかどうか
+var _closed:bool = false # 閉じるボタンを押したかどうか
+
+onready var _input_layer = $InputLayer
+
+# 数値入力開始
+func start_num_input(answer:int, var_idx:int, digit:int, auto_check:bool) -> void:
+	_mode = eMode.NUM
+	_answer_num = answer
+	_var_idx = var_idx
+	_digit = digit
+	_auto_check = auto_check
+
+func _ready() -> void:
+	# テストデータ
+	start_num_input(
+		9
+		, Adv.eVar.LVAR_00
+		, 4
+		, true)
+	
+	if Global.initialized() == false:
+		# 初期化していなかったら初期化してしまう
+		Global.init()
+
+	# 戻り値初期化
+	Global.var_set(Adv.eVar.RET, 0)
+
+# 数値の生成
+func _create_num_input() -> void:
+	var tbl = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+	
+	var px = AdvConst.WINDOW_CENTER_X
+	var py = DIAL_TOP_Y
+	var w = EscapeInputObj.WIDTH
+	px -= ((_digit * w) + (_digit - 1) * DIAL_MARGIN_W) * 0.5
+	for i in range(_digit):
+		var obj = EscapeInput.instance()
+		obj.rect_position = Vector2(px, py)
+		obj.start(tbl, 0)
+		_input_layer.add_child(obj)
+		px += w + DIAL_MARGIN_W
+
+# 正解チェック
+func _check_correct() -> bool:
+	match _mode:
+		eMode.NUM:
+			var ret = _get_input_num()
+			return ret == _answer_num
+		_:
+			# 未実装
+			return false
+
+func _get_input_num() -> int:
+	var ret:int = 0
+	var d = _digit - 1
+	for obj in _input_layer.get_children():
+		ret += obj.get_idx() * pow(10, d)
+		d -= 1
+	return ret
+
+func _process(delta: float) -> void:
+	match _state:
+		eState.INIT:
+			_update_init(delta)
+		eState.MAIN:
+			_update_main(delta)
+		eState.CORRECT:
+			_update_correct(delta)
+		eState.END:
+			# 閉じたら終了
+			queue_free()
+	
+
+# 更新 > 初期化
+func _update_init(delta:float) -> void:
+	match _mode:
+		eMode.NUM:
+			# 数値入力の生成
+			_create_num_input()
+	
+	_state = eState.MAIN
+
+# 更新 > メイン
+func _update_main(delta:float) -> void:
+	if _closed:
+		if _check_correct():
+			# 正解
+			Global.var_set(Adv.eVar.RET, 1)
+		_state = eState.END
+		return
+	
+	if _auto_check:
+		# 自動正解判定
+		if _check_correct():
+			# 正解
+			Global.var_set(Adv.eVar.RET, 1)
+			_hide_all_buttons()
+			for obj in _input_layer.get_children():
+				obj.start_blink()
+			# 正解演出
+			_state = eState.CORRECT
+			_timer = TIMER_CORRECT
+
+# 更新 > 正解
+func _update_correct(delta:float) -> void:
+	_timer -= delta
+		
+	if _timer <= 0.0:
+		_state = eState.END
+		_closed = true
+
+# 入力ボタンをすべて非表示にする
+func _hide_all_buttons() -> void:
+	$Button.hide()
+	for obj in _input_layer.get_children():
+		obj.hide_button()
+
+func _on_Button_pressed() -> void:
+	# 閉じるボタンを押した
+	_closed = true
